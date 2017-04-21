f80c092c93b209ca23a66f7e0169ce3eb7830ead
Dan Caseley <dan.caseley@myunidays.com>
2017-04-22 00:47:58 +0100

    Fix simple test

--- tests/simple.js
+++ tests/simple.js
@@ -2,7 +2,7 @@ module.exports = {
     'Simple Check' : function (browser) {
         browser
             .url("http://the-internet.herokuapp.com/login")
-            .waitForElementVisible('#login',5000)
+            .waitForElementPresent('#login',5000)
             .pause(1000) //Just for the humans watching
             .end();
     }
